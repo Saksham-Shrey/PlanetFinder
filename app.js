d1eb64a56d56bc605083a239dee68d0075454b7f
Saksham Shrey <sakshamshrey_work@icloud.com>
2023-11-01 01:24:34 +0530

    added csv-parser and habitability parameters definition

---
+++ app.js
@@ -0,0 +1,30 @@
+const {parse} = require('csv-parse');
+const fs = require('fs');
+
+const results = [];
+
+function habitablePlanets(planet){
+    return planet['koi_disposition'] === 'CONFIRMED' 
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6 ;
+}
+fs.createReadStream('KeplerData.csv')
+    .pipe(parse({
+        comment: '#',
+        columns: true
+    }))
+    .on('data', (data) => {
+        if(habitablePlanets(data)){
+        results.push(data);
+        }
+    })
+    .on('error', (err) => {
+        console.log(err);
+    })
+    .on('end', () => {
+        var number = results.length;
+       console.log(number + ' habitable planets found !!')
+    });
+
+
+//habitabilitu: koi_disposition and Stellar FLux (koi_insol) and Planetary radius (koi_prad)
